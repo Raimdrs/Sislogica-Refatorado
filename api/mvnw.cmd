c6ee383f7c973a8a1d3afea98a2b17da759e8465
Raimdrs <raiimdrs@gmail.com>
2026-08-31 17:32:43 -0300

    feat(infra): configurar monorepo com quarkus, microservico go, protos, ci/cd e documentacao

--- api/mvnw.cmd
+++ api/mvnw.cmd
@@ -2,28 +2,27 @@
 @REM Maven Start Up Batch script for Windows
 @REM ----------------------------------------------------------------------------
 @echo off
-set ERROR_CODE=0
+setlocal
 
-@REM Check for mvn in PATH
+set "PROJECT_DIR=%~dp0"
+if "%PROJECT_DIR:~-1%"=="\" set "PROJECT_DIR=%PROJECT_DIR:~0,-1%"
+
+@REM 1. If global mvn is available, use it
 where mvn >nul 2>&1
 if %ERRORLEVEL% EQU 0 (
     mvn %*
-    goto end
+    exit /B %ERRORLEVEL%
 )
 
-@REM Fallback using wrapper jar
-set "WRAPPER_JAR=%~dp0.mvn\wrapper\maven-wrapper.jar"
+@REM 2. Prepare wrapper jar
+set "WRAPPER_JAR=%PROJECT_DIR%\.mvn\wrapper\maven-wrapper.jar"
+
+if not exist "%PROJECT_DIR%\.mvn\wrapper" mkdir "%PROJECT_DIR%\.mvn\wrapper"
+
 if not exist "%WRAPPER_JAR%" (
-    if not exist "%~dp0.mvn\wrapper" mkdir "%~dp0.mvn\wrapper"
-    powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile('https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar', '%WRAPPER_JAR%')"
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile('https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar', '%WRAPPER_JAR%')"
 )
 
-java -jar "%WRAPPER_JAR%" %*
-if %ERRORLEVEL% NEQ 0 goto error
-goto end
-
-:error
-set ERROR_CODE=1
-
-:end
-exit /B %ERROR_CODE%
+@REM 3. Execute wrapper
+java -classpath "%WRAPPER_JAR%" "-Dmaven.multiModuleProjectDirectory=%PROJECT_DIR%" org.apache.maven.wrapper.MavenWrapperMain %*
+exit /B %ERRORLEVEL%
